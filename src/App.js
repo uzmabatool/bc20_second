cf0f6d200ef7939b30fa1912f0709cd89b662ff2
Uzma Batool <batool.uzma@gmail.com>
2020-07-30 21:36:23 +0800

    Updated basic React App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+function App({name, age}) {
+  return <div>
+          Hello from App.js updated by {name} Age= {age-5}
           <br/>
-          Updated By Uzma 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Hello firstname= {name}></Hello>
+        </div>
 }
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Hello.css'
+
+function Hello({firstname}) {
+return <p className="myname">This is a para in Hello tag name: {firstname}</p>
+}
+
+export default Hello;
